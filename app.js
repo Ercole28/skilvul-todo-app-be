5c8d93fc4ff7c77873193443984a7c707d0f818c
Bradley <bradleyandrewercole28@gmail.com>
2023-11-08 23:29:33 +0700

    Setting App

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require("express");
+const allRoutes = require("./routes");
+
+const app = express(); // Membuat Express App
+const PORT = process.env.PORT; // Ambil PORT dari .env
+
+app.use(express.json()); // Agar app dapat menerima json dari body disaat POST request
+app.use(allRoutes); // Ambil semua route dari dari dir Routes
+
+// Jalankan app atau server express
+app.listen(PORT, () => {
+  console.log(`Server Running on ${PORT}`);
+}) 
