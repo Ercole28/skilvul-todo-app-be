2c8907bedeff40715b3651234261bc2eadbdab6a
Bradley <bradleyandrewercole28@gmail.com>
2023-11-13 21:37:13 +0700

    Send Json Instead of Strings

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,17 +4,27 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
   try {
     const header = req.headers.authorization
-    if (!header) throw new Error("Invalid header")
+    if (!header){
+      res.status(400).json({
+        message: "Invalid Headers"
+      })
+    }
   
     const token = header.split(" ")[1]
-    if (!token) throw new Error("There is no token")
+    if (!token){
+      res.status(400).json({
+        message: "There's no Token"
+      })
+    }
   
     const user = jwt.verify(token, process.env.JWT_KEY)
     req.user = user
     
     next()
   } catch (error) {
-    res.status(400).json(error.message)
+    res.status(400).json({
+      error: error
+    })
   }
 }
 
