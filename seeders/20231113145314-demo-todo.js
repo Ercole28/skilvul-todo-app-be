1962bf75cbb76bdce4a280fd0139e3ef54689624
Bradley <bradleyandrewercole28@gmail.com>
2023-11-13 22:31:38 +0700

    Remove createdAt & updatedAt

--- seeders/20231113145314-demo-todo.js
+++ seeders/20231113145314-demo-todo.js
@@ -8,57 +8,39 @@ module.exports = {
       [
         {
           task: "Belajar React JS",
-          user_id: 1,
-          createdAt: newDate().toISOString(),
-          updatedAt: newDate().toISOString(),
+          user_id: 1
         },
         {
           task: "Belajar Next JS",
-          user_id: 1,
-          createdAt: newDate().toISOString(),
-          updatedAt: newDate().toISOString(),
+          user_id: 1
         },
         {
           task: "Belajar Express JS",
-          user_id: 2,
-          createdAt: newDate().toISOString(),
-          updatedAt: newDate().toISOString(),
+          user_id: 2
         },
         {
           task: "Belajar Node JS",
-          user_id: 2,
-          createdAt: newDate().toISOString(),
-          updatedAt: newDate().toISOString(),
+          user_id: 2
         },
         {
           task: "Belajar MySQL",
-          user_id: 2,
-          createdAt: newDate().toISOString(),
-          updatedAt: newDate().toISOString(),
+          user_id: 2
         },
         {
           task: "Belajar VueJS",
-          user_id: 3,
-          createdAt: newDate().toISOString(),
-          updatedAt: newDate().toISOString(),
+          user_id: 3
         },
         {
           task: "Belajar AngularJS",
-          user_id: 3,
-          createdAt: newDate().toISOString(),
-          updatedAt: newDate().toISOString(),
+          user_id: 3
         },
         {
           task: "Belajar LitJS",
-          user_id: 4,
-          createdAt: newDate().toISOString(),
-          updatedAt: newDate().toISOString(),
+          user_id: 4
         },
         {
           task: "Belajar EmberJS",
-          user_id: 4,
-          createdAt: newDate().toISOString(),
-          updatedAt: newDate().toISOString(),
+          user_id: 4
         },
       ],
       {}
