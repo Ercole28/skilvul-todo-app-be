4bef68f7ab7128d56f404989aec3fee0e720c484
Bradley <bradleyandrewercole28@gmail.com>
2023-11-13 21:36:44 +0700

    Renew User Model Association

--- models/user.js
+++ models/user.js
@@ -5,6 +5,10 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       // define association here
+      User.hasMany(models.Todo, {
+        foreignKey: 'user_id',
+        as: 'todos'
+      })
     }
   }
   User.init({
