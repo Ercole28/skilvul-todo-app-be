e472221d64d90c4d6e2fbff058cd556ae2e0df0d
Bradley <bradleyandrewercole28@gmail.com>
2023-11-13 23:32:52 +0700

    Add CORS

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require("express");
 const allRoutes = require("./routes");
+const cors = require('cors');
 
 const app = express(); // Membuat Express App
-const PORT = process.env.PORT; // Ambil PORT dari .env
+const PORT = process.env.PORT || 3000; // Ambil PORT dari .env
 
+app.use(cors());
 app.use(express.json()); // Agar app dapat menerima json dari body disaat POST request
 app.use(allRoutes); // Ambil semua route dari dari dir Routes
 
